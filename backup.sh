51b0b7304e4fee205652eff490fa6b38766aff28
Viacheslav_Andreev <slava.avjv@gmail.com>
2025-03-07 15:44:51 +0300

    Edit file backup

--- backup.sh
+++ backup.sh
@@ -4,8 +4,7 @@ backupDir=("/opt" "/home/ec2-user")
 backupTarget=/tmp/backup
 # Создадим директорию для архивов
 mkdir -p $backupTarget
-
-for dir in "${backupDir[@]}"; 
+for dir in "${backupDir[@]}";
 do
   backupFile="$backupTarget/$(basename "$dir")-backup-$(date +%Y-%m-%d).tar.gz"
   tar -czf  "$backupFile" "$dir"
